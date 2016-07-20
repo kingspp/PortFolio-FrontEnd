c732777bad4f66dbd55e393ea1df4d9f7db1e9d3
kingspp <kingspprathyush@gmail.com>
2016-07-20 15:28:59 +0530

    added db configuration

--- src/webapp_flask.py
+++ src/webapp_flask.py
@@ -24,6 +24,25 @@ users = {
 }
 
 
+#DB Configuration
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root@127.0.0.1/portfolio'
+db = SQLAlchemy(app)
+
+class Timeline(db.Model):
+    id = db.Column(db.Integer, primary_key=True, nullable=False, unique=True,autoincrement=True)
+    date = db.Column(db.VARCHAR(20), unique=True, nullable=False)
+    header = db.Column(db.TEXT, nullable=False)
+    body = db.Column(db.TEXT, nullable=False)
+
+    def __init__(self, date, header, body):
+        self.date = date
+        self.header = header
+        self.body = body
+
+    def __repr__(self):
+        return [self.date, self.header, self.body].__str__()
+
+
 
 
 
